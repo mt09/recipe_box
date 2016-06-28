4783d0e1797b85a701c6ca08e2fcfa385b667b6e
wen <sunzibinfa@gmail.com>
2016-06-28 15:35:44 +0800

    #edit and #update, view: edit

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -17,6 +17,20 @@ class RecipesController < ApplicationController
 		end
 	end
 
+	def edit
+		@recipe = Recipe.find(params[:id])
+	end
+
+	def update
+		@recipe = Recipe.find(params[:id])
+
+		if @recipe.update(recipe_params)
+			redirect_to recipe_path(@recipe)
+		else
+			render :edit
+		end
+	end
+
 	def show
 		@recipe = Recipe.find(params[:id])	
 	end
