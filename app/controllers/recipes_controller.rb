b14147a01d74832688c2fae7ec26ff6f6a2a8bfd
wen <sunzibinfa@gmail.com>
2016-06-28 15:02:13 +0800

    #index, #new and #create

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,2 +1,25 @@
 class RecipesController < ApplicationController
+	def index
+		@recipes = Recipe.all
+	end
+
+	def new
+		@recipe = Recipe.new
+	end
+
+	def create
+		@recipe = recipe.new(recipe_params)
+
+		if @recipe.save
+			redirect_to @recipe, warning: "Successfully created new recipe"
+		else
+			render :new
+		end
+	end
+
+	private
+
+	def recipe_params
+		params.require(:recupe).permit(:title, :description)
+	end
 end
