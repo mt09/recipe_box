bbd2e761d889bca223d3ad5f77ec5e632e18069d
wen <sunzibinfa@gmail.com>
2016-06-30 11:52:03 +0800

    修正

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -4,11 +4,11 @@ class Recipe < ActiveRecord::Base
 	has_many :ingredients
 	has_many :directions
 
-	accpets_nested_attributes_for :ingredients,
+	accepts_nested_attributes_for :ingredients,
 																reject_if: proc { |attributes| attributes["name"].blank? },
 																allow_destroy: true
 
-	accpets_nested_attributes_for :directions,
+	accepts_nested_attributes_for :directions,
 																reject_if: proc { |attributes| attributes["step"].blank? },
 																allow_destroy: true
 
